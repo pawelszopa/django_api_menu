31c9ea57c024eefdfd0a49f55fa7298b579416fe
pawelszopa <expiren@gmail.com>
2021-04-27 10:38:24 +0200

    swagger moved to main urls

--- config/urls.py
+++ config/urls.py
@@ -13,17 +13,34 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 from config import settings
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Menu API",
+        default_version='v1',
+        description="Endpoints of Car API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('v1/', include('menu.urls'), name='menu'),
     path('v1/dj-rest-auth/', include('dj_rest_auth.urls')),
     path('v1/dj-rest-auth/registration', include('dj_rest_auth.registration.urls')),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
 if settings.DEBUG:
--- menu/urls.py
+++ menu/urls.py
@@ -6,18 +6,6 @@ from rest_framework import permissions
 
 from menu import views
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Menu API",
-        default_version='v1',
-        description="Endpoints of Car API",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
-)
 
 app_name = 'menu'
 
@@ -28,5 +16,5 @@ urlpatterns = [
     path('private/dish/<int:pk>', views.DishDetailedView.as_view(), name='private_dish_detail'),
     path('public/menu/', views.PublicMenuListView.as_view(), name='public_menu'),
     path('public/menu/<int:pk>', views.PublicMenuView.as_view(), name='public_menu_detail'),
-    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+
 ]
